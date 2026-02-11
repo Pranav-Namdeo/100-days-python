32d7f7c71063b7228c94b9613af1138f2ec74ba1
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-11 22:05:07 +0530

    main.py

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,12 +1,23 @@
 from turtle import Turtle, Screen
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("red")
-for tim in range(20):
-    timmy.forward(20)
-    timmy.penup()
-    timmy.forward(20)
-    timmy.pendown()
+
+shape_data = [
+    {"angle" : "3", "color" :"red"},
+    {"angle" : "4", "color" :"blue"},
+    {"angle" : "5", "color" :"green"},
+    {"angle" : "6", "color" :"yellow"},
+    {"angle" : "7", "color" :"purple"},
+    {"angle" : "8", "color" :"orange"},
+    {"angle" : "9", "color" :"pink"},
+    {"angle" : "10", "color" :"brown"}
+]
+
+for shape in shape_data:
+    timmy.color(shape["color"])
+    for _ in range(0, int(shape["angle"])):
+        timmy.forward(100)
+        timmy.right(float(360/float(shape["angle"])))
 
 
 screen = Screen()
