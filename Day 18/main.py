e801b70776475d0f9c1ecc8ecd0b2863800279e3
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-11 21:41:20 +0530

    main.py

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,1 +1,13 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+for tim in range(20):
+    timmy.forward(20)
+    timmy.penup()
+    timmy.forward(20)
+    timmy.pendown()
 
+
+screen = Screen()
+screen.exitonclick()
