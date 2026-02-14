1241f149a060008e730b6bea6cf4d98e679c634e
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-14 17:43:06 +0530

    Update main.py

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,16 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.pensize(5)
 timmy.speed("fastest")
-colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "Gray", "Cyan", "Magenta", "Lime", "Maroon", "Navy", "Olive", "Teal", "Coral", "Turquoise", "Indigo", "Violet", "Gold", "Silver", "Beige", "Lavender", "Salmon", "Chocolate", "Crimson", "DarkBlue", "DarkGreen"]
+
+colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
 directions = [0, 90, 180, 270]
 
 for x in range(1000):
-    timmy.color(random.choice(colors))
+    timmy.color(random_color())
     timmy.setheading(random.choice(directions))
     timmy.forward(20)
+
+
 
 
 
