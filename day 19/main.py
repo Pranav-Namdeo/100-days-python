7b9ad31c55a0ab265ca7e6842b5ca3f0b32cc048
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-17 12:41:16 +0530

    Update main.py

--- day 19/main.py
+++ day 19/main.py
@@ -10,13 +10,16 @@ def turn_left():
     tim.left(10)
 def turn_right():
     tim.right(10)
+def clear_screen():
+    tim.reset()
+
 
 screen.listen()
 screen.onkey(key= "w", fun= move_forward)
 screen.onkey(key= "s", fun= move_backward)
 screen.onkey(key= "a", fun= turn_left)
 screen.onkey(key= "d", fun= turn_right)
-
+screen.onkey(key= "c", fun= clear_screen)
 
 
 
