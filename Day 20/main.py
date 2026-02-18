e192e502fe911a3dc21c81a10f66233c25d36f2f
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-18 13:09:27 +0530

    Update main.py

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,1 +1,20 @@
+from turtle import Screen, Turtle
 
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+tim = Turtle("square")
+tim.color("white")
+
+tim.shapesize(stretch_wid=1, stretch_len=3)
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
