a3940d18ac92da88734f70c03c4bb98dac38cf25
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-18 13:18:06 +0530

    Update main.py

--- Day 20/main.py
+++ Day 20/main.py
@@ -4,15 +4,22 @@ screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My snake game")
-tim = Turtle("square")
-tim.color("white")
 
-tim.shapesize(stretch_wid=1, stretch_len=3)
+start_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
+for position in start_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
 
-
-
-
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
