1b2f564371c06bb2e357af15fde1cb1c7c723ea3
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-20 20:02:22 +0530

    Update main.py

--- Day 22/main.py
+++ Day 22/main.py
@@ -5,12 +5,32 @@ screen = Screen()
 screen.setup(width= 800, height= 600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
 
+paddle_a = Turtle("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid= 5, stretch_len= 1)
+paddle_a.penup()
+paddle_a.teleport(350, 0)
 
+def go_up():
+    y = paddle_a.ycor()
+    y += 50
+    paddle_a.sety(y)
 
+def go_down():
+    y = paddle_a.ycor()
+    y -= 50
+    paddle_a.sety(y)
 
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
