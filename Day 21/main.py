d1f7931c0286cef402257dcbdddab30e0cca2acb
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-20 17:26:12 +0530

    Update main.py

--- Day 21/main.py
+++ Day 21/main.py
@@ -26,12 +26,24 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
     
+    #detech collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         score.increase_score()
+        snake.extend()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    #detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
+
+    #detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
 
 screen.exitonclick()
