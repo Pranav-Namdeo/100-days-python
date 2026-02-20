96de574053063a517559208fa32b739b183a837b
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-20 20:38:30 +0530

    Update main.py

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 screen = Screen()
@@ -10,7 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 
 
@@ -22,7 +24,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
