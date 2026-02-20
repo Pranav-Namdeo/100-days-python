a024308dd16358b16b2730255381a6c2aa330fcd
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-02-20 20:27:01 +0530

    Update main.py

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 
 screen = Screen()
@@ -7,26 +8,17 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-paddle_a = Turtle("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid= 5, stretch_len= 1)
-paddle_a.penup()
-paddle_a.teleport(350, 0)
 
-def go_up():
-    y = paddle_a.ycor()
-    y += 50
-    paddle_a.sety(y)
 
-def go_down():
-    y = paddle_a.ycor()
-    y -= 50
-    paddle_a.sety(y)
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
