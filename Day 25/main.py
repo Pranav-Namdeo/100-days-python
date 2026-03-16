3cad53cf3834d17ab4b0476c6f358d6d6774f40c
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-03-17 01:28:27 +0530

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,1 +1,9 @@
+import csv
 
+with open("./day 25/weather_data.csv") as weather_data:
+    data = csv.reader(weather_data)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+    print(temperature)
