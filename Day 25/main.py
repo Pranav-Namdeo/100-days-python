5584153689993c02bf513cc8b8763661dd752814
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-03-27 17:03:44 +0530

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -3,3 +3,13 @@ import pandas
 
 data = pandas.read_csv("./day 25/weather_data.csv")
 print(data["temp"])
+
+avg_temp = data["temp"].mean()
+#print(avg_temp)
+
+max_temp = data["temp"].max()
+print(max_temp)
+
+# Get data in rows
+
+print(data[data.temp == max_temp])
