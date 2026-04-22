3272663ec46d155b9a2a1b66bd187420bb42ea21
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-04-23 00:17:00 +0530

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,15 +1,30 @@
 import pandas
 
+#data = pandas.read_csv("./day 25/weather_data.csv")
+#print(data["temp"])
+#
+#data_dict = data.to_dict()
+#print(data_dict)
+#
+#temp_list = data["temp"].to_list()
+#print(temp_list)
+#
+#print(data["temp"].mean())
+#print(data["temp"].max())
+#
+#print(data[data.day == "Monday"])
+#
+#print(data[data.temp == data.temp.max()])
+#
+#monday = data[data.day == "Monday"]
+#monday_temp = monday.temp * 9/5 + 32
+#print(monday_temp)
 
-data = pandas.read_csv("./day 25/weather_data.csv")
-print(data["temp"])
-
-avg_temp = data["temp"].mean()
-#print(avg_temp)
-
-max_temp = data["temp"].max()
-print(max_temp)
-
-# Get data in rows
-
-print(data[data.temp == max_temp])
+# create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("./day 25/new_data.csv")
