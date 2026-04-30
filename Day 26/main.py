139ae7f3d427fa58b9f307afaaa39a01cf9e868e
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-05-01 04:41:31 +0530

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -8,5 +8,5 @@ double_no = [n * 2 for n in range(1, 11)]
 print(double_no)
 
 names = ["Peter", "Paul", "Mary", "John", "Jane", "Jack", "Jill", "James", "Jenny", "Joe"]
-short_names = [n for n in names if len(n) < 5]
+short_names = [n.upper() for n in names if len(n) < 5]
 print(short_names)
