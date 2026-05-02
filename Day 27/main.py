7a4d97e443d4ed5d700d70ea3c09b4477414a210
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-05-03 02:32:58 +0530

    Update main.py

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,1 +1,13 @@
+import tkinter
 
+window = tkinter.Tk()
+window.title("Meow")
+window.minsize(width=600, height=400)
+
+#Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+window.mainloop()
