81865863154f94db71813cbb3fded71476488109
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-05-02 17:12:02 +0530

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -21,7 +21,7 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+nato_dict = {row.letter: row.code for (index, row) in pandas.read_csv("./day 26/nato_phonetic_alphabet.csv").iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
