8e0ec1af36f8a2ca34ce0441c37bce05c35d1a04
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-05-02 17:06:11 +0530

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,26 +1,27 @@
-import random, pandas
-numbers = [1, 2, 3, 4, 5]
-squared_numbers = [n ** 2 for n in numbers]
-
-name = "Pranav"
-letters = [n for n in name]
-
-double_no = [n * 2 for n in range(1, 11)]
-
-names = ["Peter", "Paul", "Mary", "John", "Jane", "Jack", "Jill", "James", "Jenny", "Joe"]
-short_names = [n.upper() for n in names if len(n) < 5]
-
-name_score = {n: random.randint(1, 100) for n in names}
-
-passed_names = {n: s for n, s in name_score.items() if s >= 60}
-
 student_dict = {
-    "student" : ["Pranav", "Alice", "Bob", "Charlie", "David"],
-    "score" : [85, 92, 78, 88, 95]
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
 }
 
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
+#Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    if row.score >= 85:
-        print(f"{row.student} passed with a score of {row.score}")
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
