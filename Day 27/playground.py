c98c96632a5bb5ab83a85e519e8de4910c115838
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-05-07 01:42:27 +0530

    Update playground.py

--- Day 27/playground.py
+++ Day 27/playground.py
@@ -5,3 +5,10 @@ def add(*args):
     print(total)
 
 add(1, 2, 3, 4, 5, 1221)
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=50)
