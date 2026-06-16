ff0246afc8d838ed435e0f79500b5a7e90c6af70
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-06-17 00:53:17 +0530

    Update main.py

--- Day 29/main.py
+++ Day 29/main.py
@@ -3,6 +3,16 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    open(".\day 29\data.txt", "a").write(f"{website} | {email} | {password}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -19,12 +29,14 @@ website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=50)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "pranav.namdeo18@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
@@ -35,7 +47,7 @@ password_entry.grid(column=1, row=3)
 generate_pass_button = Button(text="Generate Password")
 generate_pass_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=43)
+add_button = Button(text="Add", width=43, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
