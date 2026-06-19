a299a8860031177e5b0fb37c6cdd6d100e523232
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-06-20 02:00:03 +0530

    Update main.py

--- Day 30/NATO/main.py
+++ Day 30/NATO/main.py
@@ -1,10 +1,10 @@
 import pandas
 
+data = pandas.read_csv("./day 30/NATO/nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
 def generate_phonetic():
     try:
-        data = pandas.read_csv("./day 30/NATO/nato_phonetic_alphabet.csv")
-        nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-
         word = input("Enter a word: ").upper()
         phonetic_list = [nato_dict[letter] for letter in word]
         print(phonetic_list)
