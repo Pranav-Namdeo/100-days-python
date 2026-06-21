5ab32eecd264f6b657e809b63ec8bb5753084882
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-06-21 20:06:18 +0530

    Update main.py

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,9 +1,18 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "kiromaster0@gmail.com"
 password = "xznn lwhd hffh eizz"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="anaynamdeo18@gmail.com", msg="Subject:Hello!\n\nThis is the body of my email.")
+with open("D:/Code/100 days python/day 32/quotes.txt") as file:
+    all_quotes = file.readlines()
+    quote = random.choice(all_quotes)
+
+day_of_week = dt.datetime.now().weekday()
+
+if day_of_week == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="anaynamdeo18@gmail.com", msg=f"Subject:Motivation Quote!\n\n{quote}")
