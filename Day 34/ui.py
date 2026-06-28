628a6822685511a7be35e56756a281cb6cff6e3e
Pranav Namdeo <pranav.namdeo18@gmail.com>
2026-06-28 18:54:55 +0530

    Update ui.py

--- Day 34/ui.py
+++ Day 34/ui.py
@@ -10,7 +10,7 @@ class QuizInterface():
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
 
-        self.score_label = Label(text="Score : 0", background=THEME_COLOR, fg="white")
+        self.score_label = Label(text=f"Score : 0", background=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250)
@@ -30,13 +30,28 @@ class QuizInterface():
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score : {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You reached end of the game")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_answer(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("True")
+        self.feedback(is_right)
 
     def false_answer(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("False")
+        self.feedback(is_right)
+
+    def feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
